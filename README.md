016dcf553e97949acf843f2cd3f93428bb4bcc80
Uallessonivo <uallessons@gmail.com>
2023-03-30 16:56:12 -0300

    final solution

---
+++ README.md
@@ -0,0 +1,28 @@
+﻿# C# XML Refactor
+This C# program reads data from a CSV file and an XML file, stores the data in a dictionary, and uses the data to update specific elements in the XML file. 
+
+# Installation
+To run the program, you will need to have .NET Framework installed on your machine. You can download .NET Framework from the official Microsoft website.
+
+# Usage
+Place your CSV file and XML file in the same directory (Files).
+
+Run the program by double-clicking on the executable file.
+
+The program will read the CSV file and XML file, and update specific elements in the XML file using data from the CSV file.
+
+The updated XML file will be saved in the same directory (Files).
+
+# Customization
+If you need to modify the program to work with different CSV and XML files, you can do the following:
+
+1. Edit the CSV file to contain the data you need.
+
+2. Modify the ReadCsvFile method in the Program class to read the CSV file in the correct format.
+
+3. Modify the UpdateXmlFile method in the Program class to update the XML file elements you need.
+
+4. Recompile the program into an executable file using your preferred development environment.
+
+# Credits
+This program was developed by Uallesson NUnes.
---
+++ XMLRefactor/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Xml;
+
+namespace XMLRefactor
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            Dictionary<string, string> codeBase = new Dictionary<string, string>();
+
+            using (StreamReader reader =
+                   new StreamReader(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Files", "base.csv")))
+            {
+                while (!reader.EndOfStream)
+                {
+                    string[] lines = reader.ReadLine().Split(";");
+                    if (lines.Length >= 2)
+                    {
+                        codeBase.Add(lines[0], lines[1]);
+                    }
+                }
+            }
+
+            Console.Write("Digite o nome do arquivo XML a ser modificado: ");
+            var fileName = Console.ReadLine();
+
+            XmlDocument xmlDocument = new XmlDocument();
+            xmlDocument.Load(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Files", $"{fileName}.xml"));
+
+            var nodeList = xmlDocument.GetElementsByTagName("cProd");
+
+            for (int i = 0; i < nodeList.Count; i++)
+            {
+                if (codeBase.ContainsKey(nodeList[i].InnerText))
+                {
+                    nodeList[i].InnerText = codeBase[nodeList[i].InnerText];
+                }
+                else
+                {
+                    Console.WriteLine($"O código: {nodeList[i].InnerText} não foi encontrado no arquivo Base.");
+                    Console.WriteLine($"Atualize o arquivo e execute novamente o programa...");
+                }
+            }
+
+            var numNfe = xmlDocument.GetElementsByTagName("nNF");
+            xmlDocument.Save(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Files",
+                $"{numNfe[0].InnerText}.xml"));
+        }
+    }
+}
